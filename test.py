3696834c0ec3d00abad3884a1be33e9dcdccb3ec
laihj <hj.lai@csmall.cn>
2018-05-25 14:34:15 +0800

    首次上传

---
+++ test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import jieba
+from wordcloud import WordCloud
+
+text = open(r'./4578.txt', 'r').read()
+
+cut_text = jieba.cut(text)
+
+result = "/".join(cut_text)
+
+wc = WordCloud(font_path='./msyh.ttf' ,background_color='white', width=800, height=600, max_font_size=50, max_words=1000, min_font_size=10,
+               mode='RGBA', colormap='pink')
+
+wc.generate(result)
+wc.to_file(r"./4578.png")
+
+# plt.figure("词云图")
+# plt.imshow(wc)
+# plt.axis("off")
+# plt.show()
