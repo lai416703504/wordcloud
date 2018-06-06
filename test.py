06e3bd567356604fbbf4a2bedf895b05c08ecfa9
laihj <hj.lai@csmall.cn>
2018-06-06 17:59:12 +0800

    提交

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ cut_text = jieba.cut(text)
 
 result = "/".join(cut_text)
 
-wc = WordCloud(font_path='./msyh.ttf' ,background_color='white', width=800, height=600, max_font_size=50, max_words=1000, min_font_size=10,
+wc = WordCloud(font_path='./msyh.ttf' ,background_color='white', width=1080, height=2160, max_font_size=50, max_words=10000, min_font_size=10,
                mode='RGBA', colormap='pink')
 
 wc.generate(result)
